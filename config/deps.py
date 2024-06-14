c8cf9ab9b42438d838a095d3269a489def2b43f5
Mark Veltzer <mark.veltzer@gmail.com>
2024-06-14 11:28:39 +0300

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -1,5 +1,10 @@
 packages=[
+        # the actualy runtime
         "lua5.4",
+        # linter
         "lua-check",
+        # for modules
         "luarocks",
+        # documentation
+        "lua-doc",
 ]
